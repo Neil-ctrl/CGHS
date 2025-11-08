46d3e2e8668686b71afa6c5fdfb16c705c68e858
Ashutosh <prajapatiashu777@gmail.com>
2025-11-08 23:27:37 +0530

    holyfuck

---
+++ frontend/src/components/BigFuck.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function BigFuck() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      bigesst fuck of ma life :) 
+    </>
+  )
+}
+
+export default BigFuck
